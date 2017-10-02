07299af3db3d122c88773bb68579e2084b4b30a5
krishna21august <krishnakant.singh.min12@gmail.com>
2017-10-02 11:47:40 +0530

    bookmarker app updates

--- js/main.js
+++ js/main.js
@@ -2,55 +2,85 @@
 document.getElementById('myform').addEventListener('submit',saveBookmark);
 
 
-function saveBookmark(event){
+	function saveBookmark(event){
 
-	var sitename = document.getElementById('siteName').value;
-	var siteUrl = document.getElementById('siteUrl').value;
+		var sitename = document.getElementById('siteName').value;
+		var siteUrl = document.getElementById('siteUrl').value;
 
-	var bookmark = {
-		name : sitename,
-		site : siteUrl
+		if(!sitename || !siteUrl){
+			alert('Please Fill in the form');
+			return false;
+		}
+
+		var bookmark = {
+			name : sitename,
+			site : siteUrl
+		}
+
+	    /*
+		localStorage.setItem('xcv','sdsad');
+		localStorage.getItem('xcv');
+		localStorage.removeItem('xcv');
+		*/
+
+		if(localStorage.getItem('bookmarks') === null){
+
+			bookmarks = [];
+			bookmarks.push(bookmark);
+			localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+
+		}else{
+
+			var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+			bookmarks.push(bookmark);
+			localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+		}
+	//stop form from submitting
+	   fetchbookmarks();
+	   event.preventDefault();
 	}
 
-    /*
-	localStorage.setItem('xcv','sdsad');
-	localStorage.getItem('xcv');
-	localStorage.removeItem('xcv');
-	*/
 
-	if(localStorage.getItem('bookmarks') === null){
 
-		bookmarks = [];
-		bookmarks.push(bookmark);
-		localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+	function deleteBookmark(url){
+   		//get bookmarks from localstorage
+   		var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+      
+   		//loopthrough
+   		for(var i= 0; i < bookmarks.length; i++){
+          if(bookmarks[i].site == url){
+ 		    bookmarks.splice(i,1);	
 
-	}else{
+          }
+   		}
 
-		var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-		bookmarks.push(bookmark);
-		localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+   		localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+
+   		fetchbookmarks();
+
 	}
-//stop form from submitting
-   event.preventDefault();
-}
 
 
 
-
-function fetchbookmarks(){
+	function fetchbookmarks(){
 	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
 
 	var bookmarkresults = document.getElementById('bookmarkresults');
 
     bookmarkresults.innerHTML = '';
 
-    for(i= 0; i <= bookmarks.length; i++ ){
+    for(i= 0; i < bookmarks.length; i++ ){
 
     	var name = bookmarks[i].name;
     	var url = bookmarks[i].site;
  
-    	 // bookmarkresults.innerHTML += 
+    	 bookmarkresults.innerHTML += '<div class="well">'+
+    	 							  '<h3>'+name+'<a href="'+url+'" class="btn btn-default" target = "_blank" >Visit</a>'+
+    	 							  '<a href="#" class="btn btn-danger" onclick ="deleteBookmark(\''+url+'\')" >Delete</a>'+
+    	 							  '</h3>'+
+    	 							  '</div>';
 
+    
     }
 
 }
